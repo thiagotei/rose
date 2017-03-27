7d7e88ae8454b3d9ae7bc5d3eb63cb8850ef6371
Thiago S. F. X. Teixeira <tteixei2@illinois.edu>
2017-03-27 15:35:31 -0700

    adding new tests

---
+++ tests/uiuc/continueStmt/ModTestCont.f90
@@ -0,0 +1,28 @@
+
+Module ModTestCont
+
+ CONTAINS
+
+
+     Subroutine SRT2(FcLen)
+        integer, intent(in) :: FcLen
+
+        integer i
+
+        do i = 1, FcLen
+!           if(MOD(NP,primes(p))==0) then
+!              TempFac(i) = INT(primes(p))
+!              Np = Np / primes(p)
+!              counter = counter + 1
+!           else
+              ! ... next prime number
+              p = p + 1
+              if(p > 12) continue
+!           end if
+        end do
+
+     End Subroutine SRT2
+
+     
+
+End Module ModTestCont
