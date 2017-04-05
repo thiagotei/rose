1357ff332f08a2d60fbb97daa6f5c316291e78cd
Thiago S. F. X. Teixeira <tteixei2@illinois.edu>
2017-04-04 20:04:56 -0700

    Improving the test eqvBug

--- tests/uiuc/eqvBug/ModTestEqv.f90
+++ tests/uiuc/eqvBug/ModTestEqv.f90
@@ -7,13 +7,21 @@ CONTAINS
 
     integer, intent(inout) ::ierr
     integer, optional :: opID_in
+    LOGICAL, DIMENSION(2) :: adjust 
 
    
     If (present(opID_in) .eqv. .true.) then  
         ierr = 0
+        adjust(1) = .True.
+        adjust(2) = .True.
     else
         ierr = 1
+        adjust(1) = .False.
+        adjust(2) = .False.
     end if
+    IF (adjust(2) .EQV. .TRUE.) THEN  
+        ierr = 1
+    ENDIF
 
   End Subroutine TestEqv 
 
